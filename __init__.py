fdae6b63b03a297248acd1e4abd2d4b64416dc04
Sraw <lzyl888@gmail.com>
2017-02-21 06:08:11 +0000

    bbbb

--- __init__.py
+++ __init__.py
@@ -1,4 +1,6 @@
 import psutil
+import subprocess
+import pprint
 
 def fixed_float(value, decimal):
     _str = "{0:.%sf}" % (decimal)
@@ -10,8 +12,6 @@ def convert2g(value):
     result = fixed_float(result, 2)
     return result
     
-
-
 cpu_percentage = psutil.cpu_percent(0.01, True)
 
 mem = psutil.virtual_memory()
@@ -32,7 +32,13 @@ print(cpu_percentage)
 print(mem_total)
 print(mem_available)
 print(mem_used_percentage)
-print(temperatures)
+pprint.pprint(temperatures)
 
 
 
+sp = subprocess.Popen(['nvidia-smi', '-q'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+out_str = sp.communicate()[0].decode()
+out_list = out_str.split('\n')
+
+pprint.pprint(out_list)
