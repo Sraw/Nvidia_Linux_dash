b5314feadd884be49190f5825eb8b296b60ae1a1
Sraw <lzyl888@gmail.com>
2017-02-23 06:47:56 +0000

    update

--- __init__.py
+++ __init__.py
@@ -5,32 +5,68 @@ import tornado.ioloop
 import tornado.web
 import os
 import json
+import re
+
+
 
 def fixed_float(value, decimal):
     _str = "{0:.%sf}" % (decimal)
     result = float(_str.format(value))
     return result
 
-def convert2g(value):
-    result = value / 1024 / 1024 / 1024
+def convert2g(value, mode):
+    args = {
+        "M" : 1,
+        "K" : 2,
+        "B" : 3
+    }
+    result = value / (1024 ** args[mode])
     result = fixed_float(result, 2)
     return result
+
+def get_cpu_info():
+    temperatures = psutil.sensors_temperatures()
+    cpu_percent = psutil.cpu_percent(None, True)
+    cpu_temperature = temperatures['coretemp'][1:]
+    result = {
+        "cpu_percent" : cpu_percent,
+        "cpu_temperature" : list()
+    }
+    for i in cpu_temperature:
+        result["cpu_temperature"].append([i.label, i.current])
+    return result
     
-cpu_percentage = psutil.cpu_percent(0.01, True)
-
-mem = psutil.virtual_memory()
-
-mem_total = mem.total
-
-mem_available = mem.available
-
-mem_used_percentage = fixed_float(((mem_total - mem_available) / mem_total), 2)
-
-mem_total = convert2g(mem_total)
-
-mem_available = convert2g(mem_available)
-
-temperatures = psutil.sensors_temperatures()
+def get_mem_info():
+    mem = psutil.virtual_memory()
+    mem_total = mem.total
+    mem_available = mem.available
+    mem_used_percentage = fixed_float(((mem_total - mem_available) / mem_total), 2)
+    mem_total = convert2g(mem_total, 'B')
+    mem_available = convert2g(mem_available, 'B')
+    return {
+        "mem_used_percent" : mem_used_percentage,
+        "mem_total" : mem_total,
+        "mem_available" : mem_available
+    }
+    
+def get_gpu_info():
+    sp = subprocess.Popen(['nvidia-smi', '--query-gpu=name,driver_version,temperature.gpu,utilization.gpu,memory.total,memory.free,memory.used','--format=csv'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    out_str = sp.communicate()[0].decode()
+    out_list = out_str.split('\n')
+    values = out_list[1].split(',')
+    
+    mem_total = float(re.sub(r'[^\d.]','',values[4]))
+    mem_used = float(re.sub(r'[^\d.]','',values[6]))
+    mem_used_percent = convert2g(mem_used / mem_total, 'M')
+    gpu_dict = {
+         "name" : values[0],
+         "driver_version" : re.sub(r'[^\d.]','',values[1]),
+         "temperature" : re.sub(r'[^\d.]','',values[2]),
+         "utilization" : re.sub(r'[^\d.]','',values[3]),
+         "mem_total"  : mem_total,
+         "mem_used_percent" : mem_used_percent
+    }
+    return gpu_dict
 
 class MainHandler(tornado.web.RequestHandler):
     def initialize(self):
@@ -41,21 +77,15 @@ class MainHandler(tornado.web.RequestHandler):
         self.render('index.html', title = "Nvidia-Linux-dash", cpu_percentage = cpu_percentage)
         
     def post(self):
-        cpu_percentage = psutil.cpu_percent(None, True)
-        
-        mem = psutil.virtual_memory()
-        mem_total = mem.total
-        mem_available = mem.available
-        mem_used_percentage = fixed_float(((mem_total - mem_available) / mem_total) * 100, 2)
-        mem_total = convert2g(mem_total)
-        mem_available = convert2g(mem_available)
+        cpu_info = get_cpu_info()
+        mem_info = get_mem_info()
+        gpu_info = get_gpu_info()
         
         self.set_header("content-type","application/json")
         jsobj = {
-            "cpu_percent" : cpu_percentage,
-            "mem_used_percent" : mem_used_percentage,
-            "mem_total" : mem_total,
-            "mem_available" : mem_available
+            "cpu_info" : cpu_info,
+            "mem_info" : mem_info,
+            "gpu_info" : gpu_info
         }
         self.write(json.dumps(jsobj))
         self.finish()
--- static/index.js
+++ static/index.js
@@ -1,9 +1,26 @@
 /*global $, CanvasJS*/
 
 $(function(){
-	var chart_cpu = new CanvasJS.Chart("cpu_percent",{
+	var chart_cpu_percent = new CanvasJS.Chart("cpu_percent",{
 		title :{
 			text: "CPU Usage"
+		},			
+		data: [{
+			type: "bar"
+		}],
+		axisY:{
+			maximum: 110,
+			minimum: 0,
+			interval: 10
+		},
+		axisX:{
+			interval: 1,
+		}
+	})
+	
+	var chart_cpu_temperature = new CanvasJS.Chart("cpu_temperature",{
+		title :{
+			text: "CPU Temperature"
 		},			
 		data: [{
 			type: "bar"
@@ -31,6 +48,20 @@ $(function(){
 			interval: 10
 		}
 	})
+	
+	var chart_gpu = new CanvasJS.Chart("gpu_usage",{
+		title : {
+			text: "Gpu Usage"
+		},
+		data: [{
+			type: "bar"
+		}],
+		axisY:{
+			maximum: 110,
+			minimum: 0,
+			interval: 10
+		}
+	})
 
 	var updateInterval = 1000;
 	
@@ -40,21 +71,38 @@ $(function(){
     
     function access_cpu_percent(){
         $.post('/sys_info', function(data) {
-        	//cpu
-        	var dps_cpu = new Array(data["cpu_percent"].length)
-            for(var i = 0; i < data["cpu_percent"].length; i++)
+        	//cpu_percent
+        	var cpu_percent = data["cpu_info"]["cpu_percent"]
+        	var dps_cpu_percent = new Array(cpu_percent.length)
+            for(var i = 0; i < cpu_percent.length; i++)
             {
-            	dps_cpu[data["cpu_percent"].length - i - 1] = {
-            		y : data["cpu_percent"][i],
+            	dps_cpu_percent[cpu_percent.length - i - 1] = {
+            		y : cpu_percent[i],
             		label : "CPU Core" + i.toString(),
             		indexLabel : "{y}%"
             	}
             }
-            chart_cpu.options.data[0].dataPoints = dps_cpu
+            chart_cpu_percent.options.data[0].dataPoints = dps_cpu_percent
+            
+            //cpu_temperature
+            var cpu_temperature = data["cpu_info"]["cpu_temperature"]
+            var dps_cpu_temperature = new Array(cpu_temperature.length)
+            for(var i = 0; i < cpu_temperature.length; i++)
+            {
+            	dps_cpu_temperature[cpu_temperature.length - i - 1] = {
+            		y : cpu_temperature[i],
+            		label : "Physical CPU Core" + i.toString(),
+            		indexLabel : "{y}%"
+            	}
+            }
+            chart_cpu_temperature.options.data[0].dataPoints = dps_cpu_temperature
             
             //mem
+            var mem_used_percent = data["mem_info"]["mem_used_percent"]
+            var mem_total  = data["mem_info"]["mem_total"]
+            var mem_available  = data["mem_info"]["mem_available"]
             var dps_mem = [{
-            	y : data["mem_used_percent"],
+            	y : mem_used_percent,
             	label : "Used Mem",
             	indexLabel : "{y}%"
             }]
@@ -63,8 +111,23 @@ $(function(){
             console.log(data["mem_total"])
             console.log(data["mem_available"])
             
-    		chart_cpu.render()
+    		chart_cpu_percent.render()
+    		chart_cpu_temperature.render()
     		chart_mem.render()
+    		
+    		//gpu
+    		var name = data["gpu_info"]["name"]
+    		var driver_version = data["gpu_info"]["driver_version"]
+    		var temperature = data["gpu_info"]["temperature"]
+    		var usage = data["gpu_info"]["utilization"]
+    		var mem_total = data["gpu_info"]["mem_total"]
+    		var mem_used_percent = data["gpu_info"]["mem_used_percent"]
+    		var dps_gpu = [{
+            	y : usage,
+            	label : "GPU Used",
+            	indexLabel : "{y}%"
+            }]
+            chart_gpu.options.data[0].dataPoints = dps_mem
         })
     }
 })
